cb9321af10ca52ee7313f850c688673b86052525
Pierre Grimaud <pierre.grimaud@bigyouth.fr>
2017-05-16 11:30:23 +0200

    add auth

--- src/Campaigns/Campaigns.php
+++ src/Campaigns/Campaigns.php
@@ -3,6 +3,7 @@ namespace Cheetahmail\Campaigns;
 
 class Campaigns extends \SoapClient
 {
+    const EMS_WS_NAMESPACE = 'http://ws.ems6.net/';
 
     /**
      * @var array $classmap The defined classes
@@ -91,11 +92,16 @@ class Campaigns extends \SoapClient
     );
 
     /**
+     * Campaigns constructor.
+     * @param AuthHeader $authHeader
+     *
      * @param array $options A array of config values
      * @param string $wsdl The wsdl file to use
      */
-    public function __construct($wsdl = null, $options = [])
+    public function __construct(AuthHeader $authHeader, $wsdl = null, $options = [])
     {
+        $this->__setSoapHeaders($this->createHeader($authHeader));
+
         $wsdl = $wsdl ?: __DIR__ . '/../../data/campaigns.wsdl';
 
         foreach (self::$classmap as $key => $value) {
@@ -109,6 +115,24 @@ class Campaigns extends \SoapClient
         ]);
 
         parent::__construct($wsdl, $options);
+    }
+
+    /**
+     * @param AuthHeader $authHeader
+     * @return \SoapHeader
+     */
+    public function createHeader(AuthHeader $authHeader)
+    {
+        $data = new \StdClass();
+
+        $data->UserName = new \SoapVar($authHeader->getUserName(), XSD_STRING, null, null, null, self::EMS_WS_NAMESPACE);
+        $data->Password = new \SoapVar($authHeader->getPassword(), XSD_STRING, null, null, null, self::EMS_WS_NAMESPACE);
+        $data->IdMlist  = new \SoapVar($authHeader->getIdMlist(), XSD_INTEGER, null, null, null, self::EMS_WS_NAMESPACE);
+
+        $headerValues = new \SoapVar($data, SOAP_ENC_OBJECT, null, null, null, self::EMS_WS_NAMESPACE);
+
+        $header = new \SoapHeader(self::EMS_WS_NAMESPACE, "AuthHeader", $headerValues);
+        return $header;
     }
 
     /**
